5d0c9ad3b74785e42df7a988d9a9f38e5bb84d24
Angel Newa <angelnewa101@gmail.com>
2019-12-27 05:51:51 +0545

    Initial commit

---
+++ app/src/main/java/com/example/e_softwarica3rd/adapter/StudentsAdapter.java
@@ -0,0 +1,16 @@
+package com.example.e_softwarica3rd.adapter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.e_softwarica3rd.R;
+
+public class StudentsAdapter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_students_adapter);
+    }
+}
